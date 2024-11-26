cf9b941cdad4b93afe76c06489035f9387b25972
Jonathan V <jonathanvieri1321@gmail.com>
2024-11-26 16:14:11 +0700

    Add buttons for home screen

--- View Controller/CCTVViewController.swift
+++ View Controller/CCTVViewController.swift
@@ -1,18 +1,19 @@
 //
-//  CameraViewController.swift
+//  CCTVViewController.swift
 //  Sentry
 //
-//  Created by Jonathan Vieri on 17/11/24.
+//  Created by Jonathan Vieri on 26/11/24.
 //
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CCTVViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
 
 }
